d06d0392fa5eaca5c90cb3ed2496c921cc4d4ed2
doantin135 <132345214+doantin135@users.noreply.github.com>
2025-05-14 22:44:58 +0700

    Add files via upload

--- shopr/components/shared/header/menu.tsx
+++ shopr/components/shared/header/menu.tsx
@@ -1,49 +1,20 @@
-import { EllipsisVertical } from 'lucide-react'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '../../ui/sheet'
-import CartButton from './cart-button'
-import UserButton from './user-button'
-import ThemeSwitcher from './theme-switcher'
-import LanguageSwitcher from './language-switcher'
-import { useTranslations } from 'next-intl'
+import { ShoppingCartIcon, UserIcon } from 'lucide-react'
+import Link from 'next/link'
 
-const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
-  const t = useTranslations()
+export default function Menu() {
   return (
     <div className='flex justify-end'>
-      <nav className='md:flex gap-3 hidden w-full'>
-        <LanguageSwitcher />
-        <ThemeSwitcher />
-        <UserButton />
-        {forAdmin ? null : <CartButton />}
-      </nav>
-      <nav className='md:hidden'>
-        <Sheet>
-          <SheetTrigger className='align-middle header-button'>
-            <EllipsisVertical className='h-6 w-6' />
-          </SheetTrigger>
-          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
-            <SheetHeader className='w-full'>
-              <div className='flex items-center justify-between '>
-                <SheetTitle className='  '>{t('Header.Site Menu')}</SheetTitle>
-                <SheetDescription></SheetDescription>
-              </div>
-            </SheetHeader>
-            <LanguageSwitcher />
-            <ThemeSwitcher />
-            <UserButton />
-            <CartButton />
-          </SheetContent>
-        </Sheet>
+      <nav className='flex gap-3 w-full'>
+        <Link href='/cart' className='header-button'>
+          <UserIcon className='h-8 w-8' />
+          <span className='font-bold'>Sign in</span>
+        </Link>
+
+        <Link href='/cart' className='header-button'>
+          <ShoppingCartIcon className='h-8 w-8' />
+          <span className='font-bold'>Cart</span>
+        </Link>
       </nav>
     </div>
   )
 }
-
-export default Menu
